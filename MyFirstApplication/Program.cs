291036575614ef92ccf38662b10a3b26c7d57c71
Roger O'Dell <odell.instructor@gmail.com>
2022-03-16 15:53:19 -0500

    CAC-103 C# Overview

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MyFirstApplication;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// The below statement calls a local function
+// single line comment
+HouseExample();
+
+/*
+ Multi-line comment
+The below statement is a local function. Local functions
+are inside of members and are private by default. This means
+They are specific to that member and can only be called
+inside that member.
+ */
+void HouseExample()
+{
+    House myHouse = new House();
+    myHouse.DoorOpenClose();
+}
