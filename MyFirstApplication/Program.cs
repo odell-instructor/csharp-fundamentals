a8262e6cbeca1f71ccdc4a03bb890bdddf63f735
Roger O'Dell <odell.instructor@gmail.com>
2022-03-18 11:08:17 -0500

    CAC-103 Lesson1 Value Types

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -1,11 +1,11 @@
 ﻿using MyFirstApplication;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 // The below statement calls a local function
 // single line comment
-HouseExample();
+
 
 /*
  Multi-line comment
@@ -14,8 +14,23 @@ are inside of members and are private by default. This means
 They are specific to that member and can only be called
 inside that member.
  */
+
+// Lesson Examples
+// HouseExample();
+Lesson1Example();
+
 void HouseExample()
 {
     House myHouse = new House();
     myHouse.DoorOpenClose();
+}
+
+// Lesson 1 Value Types
+void Lesson1Example()
+{
+    Lesson1 myLesson = new Lesson1();
+    myLesson.IntMinMax();
+    myLesson.IntegralExample();
+    myLesson.ConvertFloatToInt();
+    myLesson.LongFromInt();
 }
