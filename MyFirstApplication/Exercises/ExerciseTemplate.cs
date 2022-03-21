e818247ae672d1196bdd7577a4c97ff0a9841d2c
Roger O'Dell <odell.instructor@gmail.com>
2022-03-21 10:40:03 -0500

    CAC-103 Template for Exercise Labs

---
+++ MyFirstApplication/Exercises/ExerciseTemplate.cs
@@ -0,0 +1,24 @@
+﻿namespace MyFirstApplication;
+
+/*
+ The class description describes the Exercise Lab from a class level.
+ The class name should reflect the lab name... Exercise1. To get credit you
+need to provide the direct link to the exercise file from GitHub. If the
+instructions ask you to create more than 1 file, include links for each file.
+ */
+public class ExerciseTemplate
+{
+
+    /*
+     Include comments like this before each exercise number the include the
+    number assignment information. If more than one method is required, this will
+    be written once for that exercise number. The method name will be defined
+    in the lab.
+     */
+    public void ExerciseMethod()
+    {
+
+    }
+
+} // end class
+
